58fe9482e86e229061537a1eac8833f3db45f5a5
Mandar Kulkarni <mandar1308@gmail.com>
2021-01-26 12:22:09 +0530

    Changed nltk_utils file

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,43 +1,41 @@
 import numpy as np
 import nltk
-# nltk.download('punkt')
+
 from nltk.stem.porter import PorterStemmer
 stemmer = PorterStemmer()
 
 def tokenize(sentence):
     """
-    split sentence into array of words/tokens
-    a token can be a word or punctuation character, or number
+    takes a sentence and returns array of words.
+    "Hi how are you ?" => ["Hi","how","are","you","?"]
     """
     return nltk.word_tokenize(sentence)
 
 
 def stem(word):
     """
-    stemming = find the root form of the word
-    examples:
-    words = ["organize", "organizes", "organizing"]
-    words = [stem(w) for w in words]
-    -> ["organ", "organ", "organ"]
+    This function provides the root form of the word.
+    ["organize", "organizes", "organizing"] => ["organ", "organ", "organ"]
     """
     return stemmer.stem(word.lower())
 
 
 def bag_of_words(tokenized_sentence, words):
     """
-    return bag of words array:
+    returns bag of words array:
     1 for each known word that exists in the sentence, 0 otherwise
-    example:
-    sentence = ["hello", "how", "are", "you"]
+    tokenized_sentence = ["hello", "how", "are", "you"]
     words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
-    bog   = [  0 ,    1 ,    0 ,   1 ,    0 ,    0 ,      0]
+    bag of words = [0, 1, 0, 1, 0, 0, 0]
     """
-    # stem each word
-    sentence_words = [stem(word) for word in tokenized_sentence]
+    # words is stemmed while tokenized_sentence is not
+    sentence_words_stemmed = [stem(word) for word in tokenized_sentence]
     # initialize bag with 0 for each word
+    # words is array of all words in corpus. bag of words is vector
+    # representing which words our sentence has.
     bag = np.zeros(len(words), dtype=np.float32)
     for idx, w in enumerate(words):
-        if w in sentence_words: 
+        if w in sentence_words_stemmed: 
             bag[idx] = 1
 
     return bag
